432e31940d7596f745250d2a5fa4b30b45d6ab05
Ryoji <ryoji.kodakari@gmail.com>
2021-11-11 11:27:38 +0900

    articles that starts with the search word come first, changed wordnet layout, cleaned websters dictionary, libraries were upgraded

--- src/main.rs
+++ src/main.rs
@@ -10,34 +10,172 @@ extern crate rust_embed;
 #[folder = "dict"]
 struct Asset;
 
-fn print_beep_license() {
+fn print_licnse() {
     let beep_ak = Asset::get("beep/ACKNOWLEDGEMENTS").unwrap();
-    let ak = std::str::from_utf8(beep_ak.as_ref()).unwrap();
+    let ak = std::str::from_utf8(beep_ak.data.as_ref()).unwrap();
     let beep_rm = Asset::get("beep/README").unwrap();
-    let rm = std::str::from_utf8(beep_rm.as_ref()).unwrap();
+    let rm = std::str::from_utf8(beep_rm.data.as_ref()).unwrap();
     println!(
         "    ---------------------------------------------------\n{}
     ---------------------------------------------------\n{}
     ---------------------------------------------------",
         rm, ak
     );
+    let wn_lcs = Asset::get("WordNet-2.0/LICENSE").unwrap();
+    let wn = std::str::from_utf8(wn_lcs.data.as_ref()).unwrap();
+    println!(
+        "{}    ---------------------------------------------------",
+        wn
+    );
+}
+
+fn reorder<'a>(hits: &Vec<&'a String>, input: String) -> Vec<&'a String> {
+    let mut a = vec![];
+    let mut b = vec![];
+    for i in 0..hits.len() {
+        if hits[i].starts_with(input.as_str()) {
+            a.push(hits[i])
+        } else {
+            b.push(hits[i])
+        }
+    }
+    a.append(&mut b);
+    a
 }
 
 fn main() {
-    let content = std::str::from_utf8(Asset::get("websters-1913-ipa.txt").unwrap().as_ref())
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect::<Vec<String>>();
+    let wn_content = std::str::from_utf8(
+        Asset::get("wordnettsv20111111-1.tab")
+            .unwrap()
+            .data
+            .as_ref(),
+    )
+    .unwrap()
+    .lines()
+    .map(String::from)
+    .collect::<Vec<String>>();
 
-    println!(
-        "{}",
-        "########################################################################
+    let wb_content = std::str::from_utf8(
+        Asset::get("websters-1913-20111111-2.txt")
+            .unwrap()
+            .data
+            .as_ref(),
+    )
+    .unwrap()
+    .lines()
+    .map(String::from)
+    .collect::<Vec<String>>();
+
+    println!("{}", LICENSE_TEXT);
+
+    loop {
+        let input: String = get_input("");
+        if input.trim().is_empty() {
+            continue;
+        }
+        if input == ":license" {
+            print_licnse();
+            continue;
+        }
+
+        for content in &[&wn_content, &wb_content] {
+            let res = filter(&content, &input);
+            let mut child = Command::new("less")
+                .arg("-R")
+                .arg("-M")
+                .arg("+Gg")
+                .arg("-s")
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap();
+            if child
+                .stdin
+                .as_mut()
+                .ok_or("Child process stdin has not been captured!")
+                .unwrap()
+                .write_all(res.join("\n").as_bytes())
+                .is_err()
+            {}
+
+            if let Err(why) = child.wait() {
+                panic!("{}", why)
+            }
+        }
+    }
+}
+
+fn filter(content: &[String], input: &str) -> Vec<String> {
+    let high_light_left = format!(
+        "\x1b[0m\x1b[1;32m{}\x1b[0m\x1b[1;36m",
+        input.replace("\t", "")
+    );
+    let high_light_right = format!("\x1b[1;32m{}\x1b[0m", input);
+
+    let hits = content
+        .par_iter()
+        .filter(|l| l.contains(input))
+        .collect::<Vec<&String>>();
+
+    reorder(&hits, input.to_string())
+        .iter()
+        .map(|l| {
+            let tabi = l.find('\t').unwrap();
+            let left = &l[0..tabi];
+            let right = &l[tabi + 1..];
+            format!(
+                "\x1b[1;36m{}\x1b[0m  {}",
+                left.replace(&input.replace("\t", ""), &high_light_left),
+                right
+                    .replace("\\n", "\n")
+                    .replace("<ħ>", "\x1b[9m")
+                    .replace("</ħ>", "\x1b[0m")
+                    .replace(&input, &high_light_right)
+            )
+        })
+        .collect::<Vec<String>>()
+}
+
+fn get_input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_goes_into_input_above) => {}
+        Err(_no_updates_is_fine) => {}
+    }
+    input
+        .replace("\t", "📙")
+        .replace(" ", "🍵")
+        .trim()
+        .replace("🍵", " ")
+        .replace("📙", "\t")
+}
+
+pub const LICENSE_TEXT: &str =
+    "########################################################################
 #                                                                      #
 #                 Webster's Dictionary 1913 Edition                    #
 #                                                                      #
 #    en.wiktionary.org/wiki/Wiktionary:Webster%27s_Dictionary,_1913    #
 #                                                                      #
+#    --------------------------------------------------------------    #
+#                                                                      #
+#                            WordNet 2.0                               #
+#                                                                      #
+#                  https://wordnet.princeton.edu/                      #
+#                                                                      #
+#    --------------------------------------------------------------    #
+# License and Commercial Use of WordNet                                #
+#                                                                      #
+# WordNet® is unencumbered, and may be used in commercial applications #
+# in accordance with the following license agreement. An attorney      #
+# representing the commercial interest should review this WordNet      #
+# license with respect to the intended use.                            #
+#                                                                      #
+# WordNet License                                                      #
+#                                                                      #
+# This license is available as the file LICENSE in any downloaded      #
+# version of WordNet.                                                  #
 #    --------------------------------------------------------------    #
 # BEEP dictionary                                                      #
 #                                                                      #
@@ -63,83 +201,4 @@ Key in like \x1b[0m\x1b[1;32mDictionary\x1b[0m↵ or
 \x1b[0m\x1b[1;32mDictionary\x1b[1;33m\\t\x1b[0m↵ (less results).
 \x1b[1;36mCtrl+c\x1b[0m to exit.
 Type \x1b[1;33m:license\x1b[0m↵ to print that on the screen.
-"
-    );
-
-    loop {
-        let input: String = get_input("");
-        if input.trim().is_empty() {
-            continue;
-        }
-        if input == ":license" {
-            print_beep_license();
-            continue;
-        }
-
-        let high_light_left = format!(
-            "\x1b[0m\x1b[1;32m{}\x1b[0m\x1b[1;36m",
-            input.replace("\t", "")
-        );
-        let high_light_right = format!("\x1b[1;32m{}\x1b[0m", input);
-        let res = content
-            .par_iter()
-            .filter(|l| l.contains(&input))
-            .map(|l| {
-                let tabi = l.find('\t').unwrap();
-                let left = &l[0..tabi];
-                let right = &l[tabi + 1..];
-                format!(
-                    "\x1b[1;36m{}\x1b[0m  {}",
-                    left.replace(&input.replace("\t", ""), &high_light_left),
-                    right
-                        .replace("\\n", "\n")
-                        .replace("<ħ>", "\x1b[9m")
-                        .replace("</ħ>", "\x1b[0m")
-                        .replace(&input, &high_light_right)
-                )
-            })
-            .collect::<Vec<String>>();
-
-        let mut child = Command::new("less")
-            .arg("-R")
-            .arg("-M")
-            .arg("+Gg")
-            .arg("-s")
-            .stdin(Stdio::piped())
-            .spawn()
-            .unwrap();
-
-        match child
-            .stdin
-            .as_mut()
-            .ok_or("Child process stdin has not been captured!")
-            .unwrap()
-            .write_all(res.join("\n").as_bytes())
-        {
-            Err(_) => (),
-            Ok(_) => (),
-        }
-
-        match child.wait() {
-            Err(why) => panic!("{}", why),
-            Ok(_) => (),
-        }
-    }
-}
-
-fn get_input(prompt: &str) -> String {
-    print!("{}", prompt);
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_goes_into_input_above) => {}
-        Err(_no_updates_is_fine) => {}
-    }
-    input
-        .replace("\t", "📙")
-        .replace(" ", "🍵")
-        .trim()
-        .replace("🍵", " ")
-        .replace("📙", "\t")
-        .to_string()
-}
+";
